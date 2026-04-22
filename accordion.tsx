724dfcaf4039465a90a4755f07e002fa8d4a55c4
aurelius206-beep <aurelius206@gmail.com>
2026-04-21 23:20:15 -0700

    Update accordion.tsx

--- accordion.tsx
+++ accordion.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+<!-- deploy trigger -->
 import * as React from 'react'
 import * as AccordionPrimitive from '@radix-ui/react-accordion'
 import { ChevronDownIcon } from 'lucide-react'
